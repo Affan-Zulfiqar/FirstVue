c119edc137fe76812bd2a2ef36a3a9775d3068f7
Affan-Zulfiqar <affan.zulfiqar313@gmail.com>
2023-03-08 17:52:50 +0000

    work

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -2,7 +2,8 @@
     <div class="header-1 flex flex-col bg-gray-200 ">
         <nav class="bg-gray-800 px-4 py-2 flex flex-col lg:flex-row lg:items-center flex-shrink-0">
             <div class="flex justify-between items-center lg:mr-32">
-                <span class="text-white text-xl">FWR</span>
+                <span class="text-white text-xl">
+                    <RouterLink to="/">FWR</RouterLink></span>
                 <button class="border border-white px-2 py-1 rounded text-white opacity-50 hover:opacity-75 lg:hidden"
                     id="navbar-toggle">
                     <i class="fas fa-bars"></i>
@@ -25,13 +26,25 @@
                         </div>
                     </li>
                     <li>
-                        <a href="#" class="block text-gray-500 hover:text-gray-300 py-2 md:mx-2">Features</a>
+                    <div class="block text-gray-500 hover:text-gray-300 py-2 md:mx-2">
+                            <RouterLink to="/features">
+                                Features
+                            </RouterLink>
+                        </div>
                     </li>
                     <li>
-                        <a href="#" class="block text-gray-500 hover:text-gray-300 py-2 md:mx-2">Contact</a>
+                    <div class="block text-gray-500 hover:text-gray-300 py-2 md:mx-2">
+                            <RouterLink to="/contact">
+                                Contact
+                            </RouterLink>
+                        </div>
                     </li>
                     <li>
-                        <a href="#" class="block text-gray-500 hover:text-gray-300 py-2 md:mx-2">Blog</a>
+                    <div class="block text-gray-500 hover:text-gray-300 py-2 md:mx-2">
+                            <RouterLink to="/blog">
+                                Blog
+                            </RouterLink>
+                        </div>
                     </li>
                 </ul>
                 <div class="flex my-3 lg:my-0">
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView/index.vue'
 import AboutView from '../views/AboutView/index.vue'
 import SignIn from '../views/SignIn/index.vue'
+import ContactView from '../views/ContactView/index.vue'
+import Features from '../views/Features/index.vue'
+import Blog from '../views/Blog/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +27,21 @@ const router = createRouter({
       path: '/signin',
       name: 'signin',
       component: SignIn
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
+    },
+    {
+      path: '/features',
+      name: 'features',
+      component: Features
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: Blog
     }
   ]
 })
